33f484da270849ddf23ebbed96cd04375b9b81a1
zhangjingqiang <zhangjingqiang@gmail.com>
2013-09-03 23:11:36 +0900

    changed email config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,10 @@ QiangSkills::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  config.action_mailer.default_url_options = { :host => Settings.host }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,17 +76,21 @@ QiangSkills::Application.configure do
   # config.autoflush_log = false
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
+
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.default_url_options = { :host => Settings.host }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
-    port:    587,
+    port: 587,
     domain:  Settings.domain,
     authentication: "plain",
+    enable_starttls_auto: true,
     user_name: ENV['MAILER_USER_NAME'],
-    password: ENV['MAILER_PASSWORD'],
-    enable_starttls_auto:true
+    password: ENV['MAILER_PASSWORD']
   }
 end
