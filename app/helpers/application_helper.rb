231b9af5262b4a99a0e7f75daf05f496dc4fa6d4
zhangjingqiang <zhangjingqiang@gmail.com>
2013-09-16 12:44:32 +0900

    Added avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def category(category_id = nil)
     Category.find(category_id).name
   end
+
+  def avatar_url
+    gravatar_id = Digest::MD5::hexdigest(ENV['EMAIL']).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png"
+  end
 end
