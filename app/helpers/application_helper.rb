2307a4127fcc1ef01096fc0e39e8ba6105e76428
zhangjingqiang <zhangjingqiang@gmail.com>
2014-11-08 17:16:28 +0900

    Add year helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,14 @@
 module ApplicationHelper
-  def category(category_id = nil)
-    Category.find(category_id).name
-  end
-
   def avatar_url
     gravatar_id = Digest::MD5::hexdigest(ENV['EMAIL']).downcase
     "http://gravatar.com/avatar/#{gravatar_id}.png"
+  end
+
+  def copyright_year
+    case Time.now.year
+    when 2013 then '2013'
+    else
+      '2013 - ' + Time.now.year.to_s
+    end
   end
 end
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,5 @@
 module CategoriesHelper
+  def category(category_id = nil)
+    Category.find(category_id).name
+  end
 end
