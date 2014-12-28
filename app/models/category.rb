394bf62b858f8fc21cd0f5fcf9a7fe58f3b1d58d
zhangjingqiang <zhangjingqiang@gmail.com>
2014-12-28 01:58:28 +0000

    Add category default order

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,7 @@
 class Category < ActiveRecord::Base
   has_many :skills, dependent: :destroy
 
-  validates :name, presence:true, length: { maximum: 100 }
+  validates :name, presence: true, length: { maximum: 200 }
+  
+  default_scope { order('name DESC') }
 end
